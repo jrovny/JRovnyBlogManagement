53c4ee34446ee865170b24e7e5c7327a25c91262
Jordan Rovny <rovnyjordan@gmail.com>
2022-01-22 21:38:00 -0600

    Modify to enable running Angular separately

--- src/JRovnySiteManager/Startup.cs
+++ src/JRovnySiteManager/Startup.cs
@@ -67,7 +67,7 @@ namespace JRovnySiteManager
 
                 if (env.IsDevelopment())
                 {
-                    spa.UseAngularCliServer(npmScript: "start");
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
                 }
             });
         }
