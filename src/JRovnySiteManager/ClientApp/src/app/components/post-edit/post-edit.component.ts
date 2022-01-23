e3905ce4a21164cf14ef529ea135359a61e36c53
Jordan Rovny <rovnyjordan@gmail.com>
2022-01-23 17:11:32 -0600

    Add logic to fetch post to edit from service

--- src/JRovnySiteManager/ClientApp/src/app/components/post-edit/post-edit.component.ts
+++ src/JRovnySiteManager/ClientApp/src/app/components/post-edit/post-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/models/post';
+import { PostsService } from 'src/app/services/posts.service';
 
 @Component({
   selector: 'app-post-edit',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-edit.component.scss']
 })
 export class PostEditComponent implements OnInit {
+  post: any;
 
-  constructor() { }
+  constructor(private postService: PostsService) { }
 
   ngOnInit(): void {
+    this.postService.getPostById(1).subscribe(post => {
+      console.log(post);
+      this.post = post
+    });
   }
-
 }
