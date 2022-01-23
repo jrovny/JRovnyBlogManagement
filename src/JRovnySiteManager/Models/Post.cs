5ba5ed6c24e60b9f5681da76102fb51c303f7a39
Jordan Rovny <rovnyjordan@gmail.com>
2022-01-22 22:34:59 -0600

    Add default Post model

---
+++ src/JRovnySiteManager/Models/Post.cs
@@ -0,0 +1,9 @@
+namespace JRovnySiteManager.Models
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Slug { get; set; }
+    }
+}
