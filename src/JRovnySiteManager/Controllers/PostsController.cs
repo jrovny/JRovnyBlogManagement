4695187d563e27862d1fa5e7ebe5a0daacc64348
Jordan Rovny <rovnyjordan@gmail.com>
2022-01-22 22:34:49 -0600

    Create PostsController and default endpoint

---
+++ src/JRovnySiteManager/Controllers/PostsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using JRovnySiteManager.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JRovnySiteManager.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PostsController : ControllerBase
+    {
+        public async Task<List<Post>> GetAllPostsAsync()
+        {
+            return await Task.FromResult(new List<Post> { new Post { PostId = 1, Title = "Test", Slug = "test" } });
+        }
+    }
+}
