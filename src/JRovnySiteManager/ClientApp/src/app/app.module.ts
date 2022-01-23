8e29e0d3d5f5eb4412ec158c75c15b1e16c80b06
Jordan Rovny <rovnyjordan@gmail.com>
2022-01-22 23:49:02 -0600

    Add import for Angular flex module

--- src/JRovnySiteManager/ClientApp/src/app/app.module.ts
+++ src/JRovnySiteManager/ClientApp/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -22,6 +23,7 @@ import { PostListComponent } from './components/post-list/post-list.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    FlexLayoutModule,
     MatButtonModule,
     MatIconModule,
     MatSidenavModule,
