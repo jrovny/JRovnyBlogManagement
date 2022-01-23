d6a1de63018ea7e78deb4e62988813d5d2075346
Jordan Rovny <rovnyjordan@gmail.com>
2022-01-22 23:13:55 -0600

    Add data annotations to Post class

--- src/JRovnySiteManager/Models/Post.cs
+++ src/JRovnySiteManager/Models/Post.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace JRovnySiteManager.Models
 {
+    [Table("post")]
     public class Post
     {
+        [Column("post_id")]
         public int PostId { get; set; }
+        [Column("title")]
         public string Title { get; set; }
+        [Column("slug")]
         public string Slug { get; set; }
     }
 }
