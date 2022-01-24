47cb127522eb0e26eddfcd78e7262670e253b536
Jordan Rovny <rovnyjordan@gmail.com>
2022-01-23 23:41:30 -0600

    Add service call to fetch image list

---
+++ src/JRovnySiteManager/ClientApp/src/app/models/image.ts
@@ -0,0 +1,5 @@
+export interface Image {
+  imageId: number;
+  fileName: string;
+  active: boolean;
+}
--- src/JRovnySiteManager/ClientApp/src/app/services/posts.service.ts
+++ src/JRovnySiteManager/ClientApp/src/app/services/posts.service.ts
@@ -1,14 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Image } from '../models/image';
 import { Post } from '../models/post';
 import { PostEdit } from '../models/post-edit';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PostsService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getPosts() {
     return this.http.get<Post[]>('/api/posts');
@@ -16,5 +16,9 @@ export class PostsService {
 
   getPostById(id: number) {
     return this.http.get<PostEdit>('/api/posts/' + id);
+  }
+
+  getImages() {
+    return this.http.get<Image>('/api/images');
   }
 }
