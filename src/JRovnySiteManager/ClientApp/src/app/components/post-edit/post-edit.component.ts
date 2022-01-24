9379467896b88de71420e4aa722ed29871e31ebe
Jordan Rovny <rovnyjordan@gmail.com>
2022-01-23 18:28:57 -0600

    Add default controls for post edit component

--- src/JRovnySiteManager/ClientApp/src/app/components/post-edit/post-edit.component.ts
+++ src/JRovnySiteManager/ClientApp/src/app/components/post-edit/post-edit.component.ts
@@ -1,23 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { PostsService } from 'src/app/services/posts.service';
 
 @Component({
   selector: 'app-post-edit',
   templateUrl: './post-edit.component.html',
-  styleUrls: ['./post-edit.component.scss']
+  styleUrls: ['./post-edit.component.scss'],
 })
 export class PostEditComponent implements OnInit {
-  post: any;
+  form = this.fb.group({
+    title: ['', Validators.required],
+    content: [''],
+    image: [''],
+    slug: [''],
+    published: [false, Validators.required],
+  });
 
-  constructor(private postService: PostsService, private route: ActivatedRoute) { }
+  constructor(
+    private postService: PostsService,
+    private route: ActivatedRoute,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
 
-    this.postService.getPostById(id).subscribe(post => {
-      console.log(post);
-      this.post = post
+    this.postService.getPostById(id).subscribe((post) => {
+      this.form.setValue({
+        title: post.title,
+        content: post.content,
+        image: post.image,
+        slug: post.slug,
+        published: post.published,
+      });
     });
+
+    // console.log(this.form.value);
+  }
+
+  getContentHtml() {
+    return this.form.get('content')?.value;
   }
 }
