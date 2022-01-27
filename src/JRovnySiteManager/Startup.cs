01a2189396e449aec98f36e0293e41823457823c
Jordan Rovny <rovnyjordan@gmail.com>
2022-01-26 22:23:39 -0600

    Move EF models to new folder

--- src/JRovnySiteManager/Startup.cs
+++ src/JRovnySiteManager/Startup.cs
@@ -1,4 +1,5 @@
 using JRovnySiteManager.Data;
+using JRovnySiteManager.Data.EntityFramework;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
