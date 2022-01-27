638eb2a5f6abab67a6d640d7725d94828a218887
Jordan Rovny <rovnyjordan@gmail.com>
2022-01-26 22:03:34 -0600

    Create posts data access cls and default functions

---
+++ src/JRovnySiteManager/Data/PostsDataProvider.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JRovnySiteManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JRovnySiteManager.Data
+{
+    public class PostsDataProvider
+    {
+        private readonly ApplicationDbContext _context;
+        public PostsDataProvider(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<PostSummary>> GetAllPostsAsync()
+        {
+            return await _context
+                .Posts
+                .AsNoTracking()
+                .Select(p => new PostSummary
+                {
+                    PostId = p.PostId,
+                    Title = p.Title,
+                    Slug = p.Slug,
+                    CreatedDate = p.CreatedDate
+                })
+                .ToListAsync();
+        }
+
+
+        public async Task<Post> GetByIdAsync(int id)
+        {
+            return await _context.Posts.AsNoTracking().FirstOrDefaultAsync(p => p.PostId == id);
+        }
+
+        public async Task<Post> UpdateAsync(Post post)
+        {
+            _context.Update(post);
+            await _context.SaveChangesAsync();
+
+            return post;
+        }
+
+    }
+}
