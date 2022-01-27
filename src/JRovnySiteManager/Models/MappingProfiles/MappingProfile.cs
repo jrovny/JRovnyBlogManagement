c21b45d2db0b1ee9c48e50b1af5532fd91a2f134
Jordan Rovny <rovnyjordan@gmail.com>
2022-01-26 22:51:12 -0600

    Create post detail view models

--- src/JRovnySiteManager/Models/MappingProfiles/MappingProfile.cs
+++ src/JRovnySiteManager/Models/MappingProfiles/MappingProfile.cs
@@ -7,6 +7,7 @@ namespace JRovnySiteManager.Models.MappingProfiles
         public MappingProfile()
         {
             CreateMap<Data.EntityFramework.Models.Image, Models.Image>();
+            CreateMap<Data.Models.PostDetailView, Models.PostDetailView>();
         }
     }
 }
---
+++ src/JRovnySiteManager/Models/PostDetailView.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace JRovnySiteManager.Models
+{
+    public class PostDetailView
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Image { get; set; }
+        public string Slug { get; set; }
+        public bool Published { get; set; }
+        public DateTime? PublishedDate { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
