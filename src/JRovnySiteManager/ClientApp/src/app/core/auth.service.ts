f67beb6c07f376848f778fd653adbf0de889720c
Jordan Rovny <rovnyjordan@gmail.com>
2022-04-02 16:26:22 -0500

    Add auth service file and env auth settings

---
+++ src/JRovnySiteManager/ClientApp/src/app/core/auth.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { User, UserManager } from 'oidc-client-ts';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  userManager: UserManager;
+
+  constructor() {
+    const settings = {
+      authority: environment.authority,
+      client_id: environment.clientId,
+      redirect_uri: `${environment.clientRoot}signin-callback`,
+      silent_redirect_uri: `${environment.clientRoot}silent-callback.html`,
+      post_logout_redirect_uri: `${environment.clientRoot}`,
+      response_type: 'code',
+      scope: environment.scope,
+    };
+    this.userManager = new UserManager(settings);
+  }
+
+  public getUser(): Promise<User | null> {
+    return this.userManager.getUser();
+  }
+
+  public signIn(): Promise<void> {
+    return this.userManager.signinRedirect();
+  }
+
+  public renewToken(): Promise<User | null> {
+    return this.userManager.signinSilent();
+  }
+
+  public signOut(): Promise<void> {
+    return this.userManager.signoutRedirect();
+  }
+}
