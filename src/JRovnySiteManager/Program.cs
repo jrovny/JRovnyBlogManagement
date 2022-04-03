d2dc15edbb78fa19afd24ed1c7d7d654bda8ff21
Jordan Rovny <rovnyjordan@gmail.com>
2022-04-03 16:59:54 -0500

    Add missing UseEndpoints call in Program

--- src/JRovnySiteManager/Program.cs
+++ src/JRovnySiteManager/Program.cs
@@ -1,52 +1,41 @@
+using JRovnySiteManager;
 using JRovnySiteManager.Data;
 using JRovnySiteManager.Data.EntityFramework;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace JRovnySiteManager
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>();
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddTransient<PostsDataProvider>();
+builder.Services.AddTransient<ImagesDataProvider>();
+builder.Services.Configure<ApplicationSettings>(builder.Configuration.GetSection("ApplicationSettings"));
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddSpaStaticFiles(configuration =>
 {
-    public class Program
+    configuration.RootPath = "dist";
+});
+
+var app = builder.Build();
+
+app.UseSpaStaticFiles();
+app.UseRouting();
+app.UseEndpoints(endpoints => endpoints.MapControllers());
+
+// app.MapDefaultControllerRoute();
+
+app.UseSpa(spa =>
+{
+    spa.Options.SourcePath = "ClientApp";
+
+    if (app.Environment.IsDevelopment())
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            builder.Services.AddDbContext<ApplicationDbContext>();
-            builder.Services.AddAutoMapper(typeof(Program));
-            builder.Services.AddTransient<PostsDataProvider>();
-            builder.Services.AddTransient<ImagesDataProvider>();
-            builder.Services.Configure<ApplicationSettings>(builder.Configuration.GetSection("ApplicationSettings"));
-            builder.Services.AddControllersWithViews();
-            builder.Services.AddSpaStaticFiles(configuration =>
-            {
-                configuration.RootPath = "dist";
-            });
-
-            var app = builder.Build();
-
-            // app.UseHttpsRedirection();
-
-            // app.UseStaticFiles();
-            app.UseSpaStaticFiles();
-
-            app.UseRouting();
-
-            // app.UseAuthorization();
-
-            app.UseSpa(spa =>
-            {
-                spa.Options.SourcePath = "ClientApp";
-
-                if (app.Environment.IsDevelopment())
-                {
-                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
-                }
-            });
-
-            //app.MapRazorPages();
-
-            app.Run();
-        }
+        spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
     }
-}
+});
+
+app.Run();
