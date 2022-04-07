3b17ddbd7e165c4653e372953d891e8b5336e5f2
Jordan Rovny <rovnyjordan@gmail.com>
2022-04-07 00:41:52 -0500

    Change clientId to GUID

--- src/JRovnyBlogManagement/ClientApp/src/environments/environment.ts
+++ src/JRovnyBlogManagement/ClientApp/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   authority: 'https://test.accounts.jrovny.com',
-  clientId: 'test',
+  clientId: '01DAB8AD-27A8-4046-898D-31077F9F4F06',
   clientRoot: 'https://localhost:5001',
   scope: 'openid profile',
 };
