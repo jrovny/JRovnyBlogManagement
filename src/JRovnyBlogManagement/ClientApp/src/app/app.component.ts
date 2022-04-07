97bd2a0bff701e83ea0ed008f306ef8fbf5c2bb4
Jordan Rovny <rovnyjordan@gmail.com>
2022-04-07 00:10:37 -0500

    Add sign-in and sign-out buttons and logic

--- src/JRovnyBlogManagement/ClientApp/src/app/app.component.ts
+++ src/JRovnyBlogManagement/ClientApp/src/app/app.component.ts
@@ -1,16 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'oidc-client-ts';
+import { AuthService } from './core/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   sidenavOpened = true;
+  user: User | null;
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+    this.authService.getUser().then((user) => {
+      console.log('authService.getUser()', user);
+      this.user = user;
+    });
+  }
 
   toggleSidenav() {
     this.sidenavOpened = !this.sidenavOpened;
+  }
+
+  signIn() {
+    this.authService.signIn();
+  }
+
+  signOut() {
+    this.authService.signOut();
   }
 }
