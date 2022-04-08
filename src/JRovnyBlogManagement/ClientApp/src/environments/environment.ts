f60ff2f84805cd332f0e4bd716e6de04ef9f7448
Jordan Rovny <rovnyjordan@gmail.com>
2022-04-08 00:22:52 -0500

    Add API scope to env file

--- src/JRovnyBlogManagement/ClientApp/src/environments/environment.ts
+++ src/JRovnyBlogManagement/ClientApp/src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   authority: 'https://test.accounts.jrovny.com',
   clientId: '01DAB8AD-27A8-4046-898D-31077F9F4F06',
   clientRoot: 'https://localhost:5001',
-  scope: 'openid profile',
+  scope: 'openid profile JRovnyBlog.Development.All',
 };
