054499b3399f675c13c682dcef1cae4f0e304f7b
Jordan Rovny <rovnyjordan@gmail.com>
2022-04-08 00:49:46 -0500

    Add logger dependency to Posts controller

--- src/JRovnyBlogManagement/Controllers/PostsController.cs
+++ src/JRovnyBlogManagement/Controllers/PostsController.cs
@@ -3,6 +3,7 @@ using JRovny.BlogManagement.Data;
 using JRovny.BlogManagement.Data.EntityFramework.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -16,15 +17,23 @@ namespace JRovny.BlogManagement.Controllers
     {
         private readonly PostsDataProvider _dataProvider;
         private readonly IMapper _mapper;
-        public PostsController(PostsDataProvider dataProvider, IMapper mapper)
+        private readonly ILogger<PostsController> _logger;
+
+        public PostsController(
+            PostsDataProvider dataProvider,
+            IMapper mapper,
+            ILogger<PostsController> logger)
         {
             _mapper = mapper;
+            _logger = logger;
             _dataProvider = dataProvider;
         }
 
         [HttpGet]
         public async Task<IEnumerable<Models.PostSummary>> GetAllPostsAsync()
         {
+            _logger.LogInformation("Getting all blog posts");
+
             return _mapper.Map<IEnumerable<Models.PostSummary>>(await _dataProvider.GetAllPostsAsync());
         }
 
