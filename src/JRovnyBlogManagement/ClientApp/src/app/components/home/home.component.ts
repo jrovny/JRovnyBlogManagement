0da266620d845d7bf7804cfb0786eb8fb71aa9ad
Jordan Rovny <rovnyjordan@gmail.com>
2022-04-09 16:21:38 -0500

    Add check to see if user already signed in

--- src/JRovnyBlogManagement/ClientApp/src/app/components/home/home.component.ts
+++ src/JRovnyBlogManagement/ClientApp/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
@@ -7,9 +8,16 @@ import { AuthService } from 'src/app/core/auth.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.authService.getUser().then((user) => {
+      if (user) {
+        this.authService.user = user;
+        this.router.navigate(['/admin']);
+      }
+    });
+  }
 
   signIn() {
     this.authService.signIn();
