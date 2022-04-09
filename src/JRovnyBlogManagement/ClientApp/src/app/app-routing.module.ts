fcc23d2cd4430f327c04517c59d459a4a975cd99
Jordan Rovny <rovnyjordan@gmail.com>
2022-04-09 00:50:05 -0500

    Modify routes to accommodate home component

--- src/JRovnyBlogManagement/ClientApp/src/app/app-routing.module.ts
+++ src/JRovnyBlogManagement/ClientApp/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { PostEditComponent } from './components/post-edit/post-edit.component';
 import { PostListComponent } from './components/post-list/post-list.component';
 import { SigninCallbackComponent } from './components/signin-callback/signin-callback.component';
 
 const routes: Routes = [
-  { path: '', component: PostListComponent },
+  { path: '', component: HomeComponent },
+  { path: 'posts', component: PostListComponent },
   { path: 'posts/:id', component: PostEditComponent },
   { path: 'signin-callback', component: SigninCallbackComponent },
 ];
