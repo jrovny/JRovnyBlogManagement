99098b0751af53d3f778e48806847f20436793b1
Jordan Rovny <rovnyjordan@gmail.com>
2022-04-08 21:20:11 -0500

    Add auth processing to posts list

--- src/JRovnyBlogManagement/ClientApp/src/app/components/post-list/post-list.component.ts
+++ src/JRovnyBlogManagement/ClientApp/src/app/components/post-list/post-list.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from 'src/app/models/post';
 import { PostsService } from 'src/app/services/posts.service';
+import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
-  styleUrls: ['./post-list.component.scss']
+  styleUrls: ['./post-list.component.scss'],
 })
 export class PostListComponent implements OnInit {
   dataSource: Post[] = [];
   displayedColumns = ['title', 'slug', 'createdDate', 'edit'];
 
-  constructor(private postsService: PostsService) { }
+  constructor(
+    private postsService: PostsService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
-    this.postsService.getPosts().subscribe(posts => this.dataSource = posts);
+    this.authService.getUser().then((user) => {
+      this.authService.getUser().then((user) => {
+        this.authService.user = user;
+      });
+    });
+    this.postsService
+      .getPosts()
+      .subscribe((posts) => (this.dataSource = posts));
   }
-
 }
