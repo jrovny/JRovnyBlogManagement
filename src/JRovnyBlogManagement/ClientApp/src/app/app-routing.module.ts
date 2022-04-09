55307eb70b6251f75b7a8aa776e92dc10083ae38
Jordan Rovny <rovnyjordan@gmail.com>
2022-04-09 15:21:06 -0500

    Add signin-callback back as child route

--- src/JRovnyBlogManagement/ClientApp/src/app/app-routing.module.ts
+++ src/JRovnyBlogManagement/ClientApp/src/app/app-routing.module.ts
@@ -4,17 +4,19 @@ import { HomeComponent } from './components/home/home.component';
 import { PostEditComponent } from './components/post-edit/post-edit.component';
 import { PostListComponent } from './components/post-list/post-list.component';
 import { SigninCallbackComponent } from './components/signin-callback/signin-callback.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
+    canActivate: [AuthGuard],
     children: [
       { path: '', component: HomeComponent },
       { path: 'posts', component: PostListComponent },
       { path: 'posts/:id', component: PostEditComponent },
+      { path: 'signin-callback', component: SigninCallbackComponent },
     ],
   },
-  { path: 'signin-callback', component: SigninCallbackComponent },
 ];
 
 @NgModule({
