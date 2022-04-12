bbee7ef797dfe39ef5b50a6f3975714e2136fb6f
Jordan Rovny <rovnyjordan@gmail.com>
2022-04-11 23:39:09 -0500

    Add auth service to signout component

--- src/JRovnyBlogManagement/ClientApp/src/app/components/signout/signout.component.ts
+++ src/JRovnyBlogManagement/ClientApp/src/app/components/signout/signout.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
   selector: 'app-signout',
   templateUrl: './signout.component.html',
-  styleUrls: ['./signout.component.scss']
+  styleUrls: ['./signout.component.scss'],
 })
 export class SignoutComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  signIn() {
+    this.authService.signIn();
   }
-
 }
