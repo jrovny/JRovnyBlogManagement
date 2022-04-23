08d9916a8b04714267f107e646f72de7e9dfc873
Jordan Rovny <rovnyjordan@gmail.com>
2022-04-23 12:52:23 -0500

    Add config settings for staging environment

---
+++ src/JRovnyBlogManagement/ClientApp/src/environments/environment.staging.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  authority: 'https://test.accounts.jrovny.com',
+  clientId: 'f5d208d8-4dd6-40dd-9607-1c47e062968e',
+  clientRoot: 'https://test.portal.jrovny.com',
+  scope: 'openid profile JRovnyBlog.Staging.All',
+};
