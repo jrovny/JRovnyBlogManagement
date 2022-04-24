ffcf505052c05dbd968e82a4a89010e524d036d2
Jordan Rovny <rovnyjordan@gmail.com>
2022-04-24 14:59:14 -0500

    Add initial code to manage authentication

--- src/JRovnyBlogManagement.DesktopUI/MainWindow.xaml.cs
+++ src/JRovnyBlogManagement.DesktopUI/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using IdentityModel.OidcClient;
+using Microsoft.UI.Xaml;
+using System.Threading.Tasks;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,9 +12,42 @@ namespace JRovnyBlogManagement.DesktopUI
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        static string _authority = "https://test.accounts.jrovny.com"; // "https://demo.duendesoftware.com";
+        //static string _api = "https://demo.duendesoftware.com/api/test";
+        static OidcClient _oidcClient;
+
         public MainWindow()
         {
             this.InitializeComponent();
+        }
+
+        private static async Task Login()
+        {
+            // create a redirect URI using an available port on the loopback address.
+            // requires the OP to allow random ports on 127.0.0.1 - otherwise set a static port
+            var browser = new SystemBrowser();
+            string redirectUri = string.Format($"http://127.0.0.1:{browser.Port}");
+
+            var options = new OidcClientOptions
+            {
+                Authority = _authority,
+                ClientId = "bec79055-1fc3-4990-a8c0-165fec5cd03f",
+                RedirectUri = redirectUri,
+                Scope = "openid profile JRovnyBlog.Staging.All offline_access",
+                FilterClaims = false,
+                Browser = browser,
+            };
+
+            _oidcClient = new OidcClient(options);
+            var result = await _oidcClient.LoginAsync(new LoginRequest());
+
+            //ShowResult(result);
+            //await NextSteps(result);
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await Login();
         }
     }
 }
