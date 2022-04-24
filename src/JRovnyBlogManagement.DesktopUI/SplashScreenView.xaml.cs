219a2a91b905e3e76786e15afa8ba190aa671d69
Jordan Rovny <rovnyjordan@gmail.com>
2022-04-24 16:20:30 -0500

    Add splash screen window

---
+++ src/JRovnyBlogManagement.DesktopUI/SplashScreenView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using JRovnyBlogManagement.DesktopUI.Events;
+using Microsoft.Toolkit.Mvvm.Messaging;
+using Microsoft.UI.Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace JRovnyBlogManagement.DesktopUI
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SplashScreenView : Window
+    {
+        private readonly AuthenticationService _authenticationService;
+
+        public SplashScreenView(AuthenticationService authenticationService)
+        {
+            InitializeComponent();
+            _authenticationService = authenticationService;
+        }
+
+        private async void SignIn_Click(object sender, RoutedEventArgs e)
+        {
+            var result = await _authenticationService.Login();
+
+            if (result is not null)
+            {
+                WeakReferenceMessenger.Default.Send(new UserSignedInEvent());
+            }
+        }
+    }
+}
