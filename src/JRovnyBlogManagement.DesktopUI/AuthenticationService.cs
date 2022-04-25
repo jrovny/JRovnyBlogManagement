642ac2cec8125fc36a5bdf2aea8ecee053f2432a
Jordan Rovny <rovnyjordan@gmail.com>
2022-04-24 23:05:18 -0500

    Set OIDC client in constructor

--- src/JRovnyBlogManagement.DesktopUI/AuthenticationService.cs
+++ src/JRovnyBlogManagement.DesktopUI/AuthenticationService.cs
@@ -14,26 +14,12 @@ namespace JRovnyBlogManagement.DesktopUI
 
         public AuthenticationService()
         {
+            SetOidcClient();
             _localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
         }
 
         public async Task<bool> SignInAsync()
         {
-            var browser = new SystemBrowser();
-            string redirectUri = string.Format($"http://127.0.0.1:{browser.Port}");
-
-            var options = new OidcClientOptions
-            {
-                Authority = "https://test.accounts.jrovny.com",
-                ClientId = "bec79055-1fc3-4990-a8c0-165fec5cd03f",
-                RedirectUri = redirectUri,
-                Scope = "openid profile JRovnyBlog.Staging.All offline_access",
-                FilterClaims = false,
-                Browser = browser,
-            };
-
-            _oidcClient = new OidcClient(options);
-
             if (!(await RefreshTokenAsync()))
             {
                 var result = await _oidcClient.LoginAsync(new LoginRequest());
@@ -50,6 +36,24 @@ namespace JRovnyBlogManagement.DesktopUI
             }
 
             return true;
+        }
+
+        private static void SetOidcClient()
+        {
+            var browser = new SystemBrowser();
+            string redirectUri = string.Format($"http://127.0.0.1:{browser.Port}");
+
+            var options = new OidcClientOptions
+            {
+                Authority = "https://test.accounts.jrovny.com",
+                ClientId = "bec79055-1fc3-4990-a8c0-165fec5cd03f",
+                RedirectUri = redirectUri,
+                Scope = "openid profile JRovnyBlog.Staging.All offline_access",
+                FilterClaims = false,
+                Browser = browser,
+            };
+
+            _oidcClient = new OidcClient(options);
         }
 
         private async Task<bool> RefreshTokenAsync()
@@ -72,10 +76,10 @@ namespace JRovnyBlogManagement.DesktopUI
 
         public async Task<string> AccessToken()
         {
-            // TODO: Check access token expiration here
+            // Check access token expiration here
             if (AccessTokenExpired())
             {
-                await RefreshTokenAsync();
+                await SignInAsync();
             }
 
             return _accessToken;
