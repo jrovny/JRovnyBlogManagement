e5de08007eb382fd6f09f3133821efb90fb451ae
Jordan Rovny <rovnyjordan@gmail.com>
2022-04-25 00:04:34 -0500

    Add PostSummay class

---
+++ src/JRovnyBlogManagement.DesktopUI/PostSummary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JRovnyBlogManagement.DesktopUI
+{
+    public class PostSummary
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
