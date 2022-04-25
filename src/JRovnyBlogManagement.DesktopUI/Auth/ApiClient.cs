462c28b12c41ce875fe4ded7575eb98573d826e4
Jordan Rovny <rovnyjordan@gmail.com>
2022-04-24 23:35:21 -0500

    Add API client class

---
+++ src/JRovnyBlogManagement.DesktopUI/Auth/ApiClient.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http;
+
+namespace JRovnyBlogManagement.DesktopUI.Auth
+{
+    public class ApiClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApiClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+    }
+}
