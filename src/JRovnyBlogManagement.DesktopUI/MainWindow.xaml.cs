44a3bcfcc758101fb1ed9a4a98bc87fe8df7d62d
Jordan Rovny <rovnyjordan@gmail.com>
2022-04-25 00:12:38 -0500

    Add API endpoint test call

--- src/JRovnyBlogManagement.DesktopUI/MainWindow.xaml.cs
+++ src/JRovnyBlogManagement.DesktopUI/MainWindow.xaml.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.UI.Xaml;
+﻿using JRovnyBlogManagement.DesktopUI.Auth;
+using Microsoft.UI.Xaml;
+using System.Collections.Generic;
 
 namespace JRovnyBlogManagement.DesktopUI
 {
     public sealed partial class MainWindow : Window
     {
-        public MainWindow()
+        private readonly ApiClient _apiClient;
+
+        public MainWindow(ApiClient apiClient)
         {
             InitializeComponent();
+            this._apiClient = apiClient;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var results = await _apiClient.GetAsync<IEnumerable<PostSummary>>(
+                new System.Uri("/api/posts", System.UriKind.Relative));
         }
     }
 }
