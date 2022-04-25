9b9a90d05937bf7d3be2a3ae13b088a23548463b
Jordan Rovny <rovnyjordan@gmail.com>
2022-04-25 00:04:17 -0500

    Add generic func method to API client

--- src/JRovnyBlogManagement.DesktopUI/Auth/ApiClient.cs
+++ src/JRovnyBlogManagement.DesktopUI/Auth/ApiClient.cs
@@ -1,14 +1,36 @@
-﻿using System.Net.Http;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace JRovnyBlogManagement.DesktopUI.Auth
 {
     public class ApiClient
     {
-        private readonly HttpClient _httpClient;
+        private readonly HttpClient _client;
 
         public ApiClient(HttpClient httpClient)
         {
-            _httpClient = httpClient;
+            _client = httpClient;
+        }
+
+        public async Task<T> GetAsync<T>(Uri location, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            using var response = await _client.SendAsync(
+                new HttpRequestMessage(HttpMethod.Get, location.ToString()),
+                HttpCompletionOption.ResponseHeadersRead,
+                cancellationToken);
+            response.EnsureSuccessStatusCode();
+
+            using var streamReader = new StreamReader(await response.Content.ReadAsStreamAsync());
+            using var jsonTextReader = new JsonTextReader(streamReader);
+            var jsonSerializer = new JsonSerializer();
+
+            return jsonSerializer.Deserialize<T>(jsonTextReader);
         }
     }
 }
