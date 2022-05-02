64b0b615099fe1867b089dffa82a7ba9aa860a6f
Jordan Rovny <rovnyjordan@gmail.com>
2022-05-02 00:23:06 -0500

    Create MSAL redirect component

---
+++ src/JRovnyBlogManagement/ClientApp/src/app/components/msal-redirect/msal-redirect.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-msal-redirect',
+  templateUrl: './msal-redirect.component.html',
+  styleUrls: ['./msal-redirect.component.scss']
+})
+export class MsalRedirectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
