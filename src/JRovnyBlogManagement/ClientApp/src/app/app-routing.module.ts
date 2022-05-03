b0f0ca9efa5e99fe464579b855f9eb729c4f6b43
Jordan Rovny <rovnyjordan@gmail.com>
2022-05-03 17:46:52 -0500

    Add initialNavigation property to router

--- src/JRovnyBlogManagement/ClientApp/src/app/app-routing.module.ts
+++ src/JRovnyBlogManagement/ClientApp/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BrowserUtils } from '@azure/msal-browser';
 import { HomeComponent } from './components/home/home.component';
 import { PostEditComponent } from './components/post-edit/post-edit.component';
 import { PostListComponent } from './components/post-list/post-list.component';
@@ -8,21 +9,29 @@ import { SilentCallbackComponent } from './components/silent-callback/silent-cal
 import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent,
-    canActivate: [AuthGuard],
-    children: [
-      { path: 'signin-callback', component: SigninCallbackComponent },
-      { path: 'silent-callback.html', component: SilentCallbackComponent },
-      { path: 'posts', component: PostListComponent },
-      { path: 'posts/:id', component: PostEditComponent },
-    ],
-  },
+  // {
+  // path: '',
+  // component: HomeComponent,
+  // canActivate: [AuthGuard],
+  // children: [
+  { path: 'signin-callback', component: SigninCallbackComponent },
+  { path: 'silent-callback.html', component: SilentCallbackComponent },
+  { path: 'posts', component: PostListComponent },
+  { path: 'posts/:id', component: PostEditComponent },
+  //   ],
+  // },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      // Don't perform initial navigation in iframes or popups
+      initialNavigation:
+        !BrowserUtils.isInIframe() && !BrowserUtils.isInPopup()
+          ? 'enabled'
+          : 'disabled', // Remove this line to use Angular Universal
+    }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
