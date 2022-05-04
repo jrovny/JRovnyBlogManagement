4968c80f12a6bef7ae4c2e22e2e98bba02bde21f
Jordan Rovny <rovnyjordan@gmail.com>
2022-05-03 20:41:54 -0500

    Modify router to use MSAL route guard

--- src/JRovnyBlogManagement/ClientApp/src/app/app-routing.module.ts
+++ src/JRovnyBlogManagement/ClientApp/src/app/app-routing.module.ts
@@ -1,25 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MsalGuard } from '@azure/msal-angular';
 import { BrowserUtils } from '@azure/msal-browser';
 import { HomeComponent } from './components/home/home.component';
 import { PostEditComponent } from './components/post-edit/post-edit.component';
 import { PostListComponent } from './components/post-list/post-list.component';
 import { SigninCallbackComponent } from './components/signin-callback/signin-callback.component';
 import { SilentCallbackComponent } from './components/silent-callback/silent-callback.component';
-import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
-  // {
-  // path: '',
-  // component: HomeComponent,
-  // canActivate: [AuthGuard],
-  // children: [
-  { path: 'signin-callback', component: SigninCallbackComponent },
-  { path: 'silent-callback.html', component: SilentCallbackComponent },
-  { path: 'posts', component: PostListComponent },
-  { path: 'posts/:id', component: PostEditComponent },
-  //   ],
-  // },
+  {
+    path: '',
+    component: HomeComponent,
+    canActivate: [MsalGuard],
+    children: [
+      { path: 'signin-callback', component: SigninCallbackComponent },
+      { path: 'silent-callback.html', component: SilentCallbackComponent },
+      { path: 'posts', component: PostListComponent },
+      { path: 'posts/:id', component: PostEditComponent },
+    ],
+  },
 ];
 
 @NgModule({
